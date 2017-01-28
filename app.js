abd4cbc959d714000e14bdfff3283b28d261054e
louiemontes <louiemontes@gmail.com>
2017-01-28 14:26:48 -0700

    finish episode 2

--- app.js
+++ app.js
@@ -8,5 +8,17 @@ app.get('/', function(req, res) {
 app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(2000);
+console.log("Server started.");
 
+var io = require('socket.io')(serv, {});
+io.sockets.on('connect', function(socket){
+  console.log('socket connection');
 
+  socket.on('happy', function(data){
+    console.log("happy because " + data.reason);
+  });
+
+  socket.emit('serverMsg', {
+    msg: 'hello',
+  });
+});
